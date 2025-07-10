de946d27846f650ead8cdbd962317ab46327c894
kryst <krystofrnanju@gmail.com>
2025-07-10 19:43:39 +0100

    Some new text

--- app/src/main/java/com/app/git_github_demo/MainActivity.kt
+++ app/src/main/java/com/app/git_github_demo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import com.app.git_github_demo.ui.theme.GitGitHubDemoTheme
 
@@ -16,7 +17,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GitGitHubDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    Text(text = "New Text")
                 }
             }
         }
